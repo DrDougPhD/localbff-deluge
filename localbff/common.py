ee2f7a5817ca5c554c8e1a09969058c7aa6a2394
Doug McGeehan <djmvfb@mst.edu>
2013-07-03 13:48:53 -0500

    Missing import of os from common.py

--- localbff/common.py
+++ localbff/common.py
@@ -38,6 +38,8 @@
 #    this exception statement from your version. If you delete this exception
 #    statement from all source files in the program, then also delete it here.
 #
+import os
+
 
 def get_resource(filename):
     import pkg_resources
